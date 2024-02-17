5bc24eb4af7f7485f7bdc9b18c663576f8c9369e
Tohidujjaman Hoque <80402614+tj8016@users.noreply.github.com>
2024-02-18 00:21:23 +0530

    Update Work.jsx

--- src/components/core/QualificationSection/Work.jsx
+++ src/components/core/QualificationSection/Work.jsx
@@ -3,10 +3,25 @@ import React from 'react'
 export const Work = () => {
     return (
         <div>
-            {/* <!-- ======================= WORK CONTENT 2 ===================--> */}
-            <div className="qualification-content" data-content id="work">
-                <div className="flex justify-center items-center">
-                    <h1 className=" font-rubik text-subText">NO WORK EXPERIENCE</h1>
+            {/* <!-- ======================= WORK CONTENT 1 ===================--> */}
+            <div className="" data-content id="education">
+                {/* <!--==================== WORK 1====================--> */}
+                <div className="relative grid grid-cols-3">
+                    <div className="flex flex-col">
+                        <h3
+                            className="qualification__title font-rubik font-semibold text-lg text-firstColor">
+                            Software Developer intern</h3>
+                        <span className="font-rubik text-md text-secondColor">Ditinex</span>
+                        <div className="font-rubik text-sm  text-subText">
+                            <i className="uil uil-calendar-alt"></i>
+                            2024 - 2025
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center items-center pt-2">
+                        <span
+                            className="inline-block h-3 w-3 rounded-full bg-firstColor"></span>
+                        <span className="w-[2px] h-[100%] bg-firstColor"></span>
+                    </div>
                 </div>
             </div>
         </div>
